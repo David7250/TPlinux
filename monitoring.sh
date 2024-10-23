defa3809badeb43cd0ccf7a59107e7c365cead93
David7250 <jullemierd@gmail.com>
2024-10-23 21:26:40 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
 echo "Architecture:" 
-sudo uname -a
+uname -a
 
-echo "Nombre de CPU" 
-sudo cat /proc/cpuinfo | grep "cpu cores"
+echo "Nombre de CPU physique" 
+nproc --all
+
+echo "Nombre de CPU virtuelle"
+grep -c ^processor /proc/cpuinfo
 
 echo "Mémoire vive" 
-sudo free && vmstat
+free && vmstat
 
 echo "Utilisation des CPU" 
-sudo top -n1 -b | grep "%Cpu(s)"
+top -n1 -b | grep "%Cpu(s)"
 
 echo "Date et heure du dernier redemarrage" 
-sudo who -b
+who -b
 
 echo "LVM actif"  
-sudo lvscan
+lvscan
 
 echo "Nombre de connexion active"
-sudo netstat -taupn
+netstat -taupn
 
 echo "Nombre d'utilisateur du serveur"
-sudo wc -l /etc/passwd
+wc -l /etc/passwd
 
 echo "Adresse IPV4 et MAC"
-sudo ip a | inet link/ether
+ip a show | awk '/inet / {print "IPv4: " $2}' | grep -v '127.0.0.1'
+ip link show | awk '/ether/ {print "MAC: " $2}'
 
 echo "Nombre de commande sudo"
-sudo wc -l var/log/sudo.log
+sudo grep 'COMMAND' /var/log/auth.log | wc -l
